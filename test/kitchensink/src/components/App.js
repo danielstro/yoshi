4747153df53d8e945a50fa1bfd77d81223a706d1
Ronen Amiel <ronen.amiel@gmail.com>
2018-11-02 16:32:34 +0200

    remove double .chunk from chunk names

--- test/kitchensink/src/components/App.js
+++ test/kitchensink/src/components/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
 
     const {
       default: feature,
-    } = await import(/* webpackChunkName: "[request].chunk" */ `./features/${featureName}`);
+    } = await import(/* webpackChunkName: "[request]" */ `./features/${featureName}`);
 
     this.setState({ feature });
   }
