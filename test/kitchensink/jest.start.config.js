7bdedbfe76bd8f307fec50a64072b5a23d102f02
Ronen Amiel <ronen.amiel@gmail.com>
2018-11-04 11:29:47 +0200

    wip

--- test/kitchensink/jest.start.config.js
+++ test/kitchensink/jest.start.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  testMatch: ['**/webpack.test.js'],
+  testMatch: ['**/webpack.test.js', '**/hmr.test.js'],
+  testEnvironment: require.resolve('./startEnvironment'),
   transformIgnorePatterns: ['/node_modules/', '/test/'],
   globalSetup: require.resolve('./startGlobalSetup'),
   globalTeardown: require.resolve('./startGlobalTeardown'),
