d4dbbb09272c010ef658dfacf21b8d559ef247c9
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-11-11 16:50:27 +0200

    add tests for generator npm start

--- packages/create-yoshi-app/scripts/e2e.js
+++ packages/create-yoshi-app/scripts/e2e.js
@@ -7,6 +7,9 @@ const { createApp, verifyRegistry, projects } = require('../src/index');
 const prompts = require('prompts');
 const expect = require('expect');
 const { testRegistry } = require('../src/constants');
+const {
+  killSpawnProcessAndHisChildren,
+} = require('../../../test-helpers/process');
 
 // verbose logs and output
 const verbose = process.env.VERBOSE_TESTS;
@@ -61,14 +64,69 @@ const testTemplate = mockedAnswers => {
       });
     }
 
-    it(`should run npm test with no configuration warnings`, () => {
-      console.log('running npm test...');
-      const { stderr } = execa.shellSync('npm test', {
-        cwd: tempDir,
-        stdio,
+    describe('npm test', () => {
+      it(`should run npm test with no configuration warnings`, () => {
+        console.log('running npm test...');
+        const { stderr } = execa.shellSync('npm test', {
+          cwd: tempDir,
+          stdio,
+        });
+
+        expect(stderr).not.toContain('Warning: Invalid configuration object');
+      });
+    });
+
+    describe('npm start', () => {
+      let child;
+      const serverPort = 3000;
+      const cdnPort = 3200;
+      afterEach(() => {
+        return killSpawnProcessAndHisChildren(child);
+      });
+      it(`should run with local server`, async () => {
+        console.log('running npm start...');
+
+        child = execa.shell('npm start', {
+          cwd: tempDir,
+          stdio,
+        });
+
+        execa.shellSync(`npx wait-port ${serverPort} -o silent`, {
+          stdio,
+        });
+
+        const { statusCode } = await new Promise(resolve =>
+          require('http').get(`http://localhost:${serverPort}`, res => {
+            resolve(res);
+          }),
+        );
+
+        expect(statusCode).toBe(200);
       });
 
-      expect(stderr).not.toContain('Warning: Invalid configuration object');
+      it(`should run with local cdn server`, async () => {
+        console.log('running npm start...');
+
+        child = execa.shell('npm start', {
+          cwd: tempDir,
+          stdio,
+        });
+
+        execa.shellSync(`npx wait-port ${cdnPort} -o silent`, {
+          stdio,
+        });
+
+        const { statusCode } = await new Promise(resolve =>
+          require('http').get(
+            `http://localhost:${cdnPort}/app.bundle.js`,
+            res => {
+              resolve(res);
+            },
+          ),
+        );
+
+        expect(statusCode).toBe(200);
+      });
     });
   });
 };
