0e266fad05460bc7a5a8e60872f7860edb18e6e6
Ronen Amiel <ronen.amiel@gmail.com>
2018-11-15 12:06:17 +0200

    a bit of refactor

--- test/scripts.js
+++ test/scripts.js
@@ -12,26 +12,13 @@ async function waitForPort(port, { timeout = 10000 } = {}) {
   }
 }
 
-function stripYarn(output) {
-  let lines = output.split('\n');
-
-  const runIndex = lines.findIndex(line => line.match(/^yarn run/));
-
-  if (runIndex !== -1) {
-    lines.splice(0, runIndex + 2);
-    lines = lines.filter(line => !line.match(/^info Visit.*yarnpkg/));
-  }
-
-  return lines.join('\n');
-}
-
 function execaSafe(...args) {
   return execa(...args)
     .then(({ stdout, stderr, ...rest }) => ({
       fulfilled: true,
       rejected: false,
-      stdout: stripYarn(stripAnsi(stdout)),
-      stderr: stripYarn(stripAnsi(stderr)),
+      stdout: stripAnsi(stdout),
+      stderr: stripAnsi(stderr),
       ...rest,
     }))
     .catch(err => ({
@@ -39,13 +26,11 @@ function execaSafe(...args) {
       rejected: true,
       reason: err,
       stdout: '',
-      stderr: stripYarn(
-        stripAnsi(
-          err.message
-            .split(os.EOL)
-            .slice(2)
-            .join(os.EOL),
-        ),
+      stderr: stripAnsi(
+        err.message
+          .split(os.EOL)
+          .slice(2)
+          .join(os.EOL),
       ),
     }));
 }
@@ -70,6 +55,8 @@ module.exports = class Scripts {
       },
     });
 
+    // `startProcess` will never resolve but if it fails this
+    // promise will reject immediately
     await Promise.race([waitForPort(port), startProcess]);
 
     return {
